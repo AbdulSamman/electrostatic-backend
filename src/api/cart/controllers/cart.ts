e1e883f878d1d5379be427dd1bdd5c53bbbe59fd
AbdulSamman <sammanab@outlook.de>
2025-12-27 06:11:27 +0100

    createCoreControlelr added

--- src/api/cart/controllers/cart.ts
+++ src/api/cart/controllers/cart.ts
@@ -1,7 +1,33 @@
-/**
- * cart controller
- */
+import { factories } from "@strapi/strapi";
 
-import { factories } from '@strapi/strapi';
+export default factories.createCoreController(
+  "api::cart.cart",
+  ({ strapi }) => ({
+    async create(ctx) {
+      try {
+        // Daten aus dem Request holen
+        const { userName, email, products } = ctx.request.body.data;
 
-export default factories.createCoreController('api::cart.cart');
+        // Numeric IDs für Relation extrahieren
+        const productIds = products.map((p: any) => p.id);
+
+        const newCart = await strapi.db.query("api::cart.cart").create({
+          data: {
+            userName,
+            email,
+            products: productIds, // Relation korrekt speichern
+            selectedOptions: products.map((p: any) => ({
+              id: p.id,
+              qty: p.qty ?? 1,
+              selectedOptions: p.selectedOptions ?? [],
+            })),
+          },
+        });
+
+        return newCart;
+      } catch (err) {
+        ctx.throw(500, err);
+      }
+    },
+  })
+);
