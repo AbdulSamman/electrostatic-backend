9b682cddf217b55651210685a6649c9e5d5f2604
AbdulSamman <sammanab@outlook.de>
2026-01-24 17:11:09 +0100

    plugins updated

--- config/plugins.ts
+++ config/plugins.ts
@@ -7,6 +7,16 @@ export default ({ env }) => ({
         api_key: env("CLOUDINARY_KEY"),
         api_secret: env("CLOUDINARY_SECRET"),
       },
+      sizeLimit: 200 * 1024 * 1024, // 200 MB für Videos
+      allowedTypes: [
+        "image/jpeg",
+        "image/png",
+        "image/webp",
+        "image/svg+xml",
+        "video/mp4",
+        "video/webm",
+        "video/quicktime", // .mov
+      ],
       actionOptions: {
         upload: {},
         delete: {},
